7e1e1cfd31935f7714dc54a6aa458964230b6276
Stanislav Pavenko <pavenko_sv@mail.ru>
2017-09-14 10:29:02 +0300

    showmodel add datetime add

---
+++ GetingCertificatesListA2Beta16/Controls/datetime.ts
@@ -0,0 +1,21 @@
+﻿export function InitDateExt() {
+
+	Date.prototype.addDays = function(days:number){
+		let d = this.getDate() + days;
+		this.setDate(d);
+		return this;
+	}
+
+	Date.prototype.toStringRu = function () {
+
+		let d:number = this.getDate();
+		let M: number = this.getMonth();
+		let y: number = this.getFullYear();
+		let H: number = this.getHours();
+		let m: number = this.getMinutes();
+		let s: number = this.getSeconds();
+
+		return '' + d.toString(2) + '.' + M.toString(2) + '.' + y.toString(4) + ' ' + H.toString(2) + ':' + m.toString(2) + ':' + s.toString(2);
+
+	}
+}
---
+++ GetingCertificatesListA2Beta16/Controls/showmodel.ts
@@ -0,0 +1,78 @@
+﻿export interface ISelectItem {
+	id: any;
+	name: string;
+	text: string;
+	children: Array<ISelectItem>;
+}
+
+
+export function ShowError(error: any) {
+
+	if (error._body) {
+		var errObj = JSON.parse(error._body);
+		if ((errObj) && errObj.Message) {
+			//alert(errObj.Message);
+			$("#ErrorBlock").html(" " + errObj.Message);
+			$("#ErrorModalBox").modal("show");
+		}
+	}
+	else {
+		$("#ErrorBlock").html(" " + error);
+		$("#ErrorModalBox").modal("show");
+	}
+
+}
+
+export function ShowComplete() {
+	$("#CompleteModalBox").modal("show");
+
+}
+
+export function ShowMessage(message: string) {
+	$("#MessageBlock").html(message);
+	$("#MessageModalBox").modal("show");
+}
+
+export function ShowRecordInfo(crName: string, createdDate: string, luName: string, lastUpdatedDate:string) {
+
+	var recInfo = `Кем создано: <strong>${crName}</strong><br/>Когда создано: <strong>${createdDate}</strong><br/>Кем изменено: <strong>${luName}</strong><br/>Когда изменено: <strong>${lastUpdatedDate}</strong>`;
+
+	$("#RecordInfo").html(recInfo);
+	$("#RecordInfoModalBox").modal("show");
+}
+
+//Преобразует строку в дату (время отсекается)
+//В случае ошибки возвращает undefined
+export function StringToDate(str): Date {
+    let parts: Array<number> = [];
+    if (str && str.split(" ").length > 0 && str.split(" ")[0].split(".").length > 2) {
+        str.split(" ")[0].split(".").forEach((currentValue, index, array) => {
+            parts.push(parseInt(currentValue));
+        };
+        return new Date(parts[2], parts[1], parts[0]);
+    } else {
+        return undefined;
+    }
+}
+
+//Преобразует строку в дату и время 
+export function StringToDateTime(str): Date {
+    let parts: Array<number> = [];
+    if (str &&
+        str.split(" ").length == 2) {
+        str.split(" ")[0].split(".").forEach((currentValue, index, array) => {
+            parts.push(parseInt(currentValue));
+        };
+        str.split(" ")[1].split(":").forEach((currentValue, index, array) => {
+            parts.push(parseInt(currentValue));
+        };
+        return new Date(parts[2], parts[1], parts[0], parts[3], parts[4], parts[5]);
+    } else {
+        return undefined;
+    }
+}
+
+export function ShowModal(id: string) {
+    $(`#${id}`).modal("show");
+}
+
